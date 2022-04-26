d0640778adcc657d36052310abbcef39155c6208
aman26kbm <aman.kbm@gmail.com>
2022-04-25 22:45:43 -0500

    Deleting pim_sim folder. It's contents have been integrated into the rest of the codebase.

--- pim_sim/InterConnect.h
+++
@@ -1,5 +0,0 @@
-#include "util.h"
-
-uint64_t _cycle_per_word = 1;
-uint64_t _hops = 4;
-double _time_per_cycle = 1E-6;
--- pim_sim/system.h
+++
@@ -1,8 +0,0 @@
-#include "util.h"
-#include <vector>
-#include <utility>
-//#include "Request.h"
-
-uint32_t global_clk;
-uint8_t _nrow=64;
-uint8_t _ncol=64;
