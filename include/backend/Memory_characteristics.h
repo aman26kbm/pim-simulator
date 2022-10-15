7d52eb9e4d3a9f82dec3437be976c986f8317c80
aman26kbm <aman.kbm@gmail.com>
2022-10-14 19:52:09 -0500

    Updating energy values

--- include/backend/Memory_characteristics.h
+++ include/backend/Memory_characteristics.h
@@ -32,27 +32,27 @@ public:
     const TimeT T_CLK = 1; 
 
     //Energy in J spent per word read from a memory array
-    const double E_ArrayRd = 2.39e-12;
+    const double E_ArrayRd = 8.6e-12; //from NC paper
     //Energy in J spent per word written to a memory array
-    const double E_ArrayWr = 2.39e-12;
+    const double E_ArrayWr = 8.6e-12; //from NC paper
     //Energy in J spent per compute op performed in a memory array
-    const double E_ArrayCompute = 4.78e-12;
+    const double E_ArrayCompute = 17.2e-12; //read+write
     //Energy in J spent per instruction decode/execute
-    const double E_InstrCtrl = 7.66e-20;
+    const double E_InstrCtrl = 7.66e-11;
     //Energy in J spent per popcount operation (all steps)
-    const double E_Popcount = 6.29e-22;
+    const double E_Popcount = 6.29e-13;
     //Energy in J spent per RF read 
-    const double E_RfRd = 4.22e-21;
+    const double E_RfRd = 4.22e-12;
     //Energy in J spent per RF write
-    const double E_RfWr = 4.22e-21;
+    const double E_RfWr = 4.22e-12;
     //Energy in J spent in transfer a flit (1024 bits) from one router to another (1 hop)
-    const double E_NoC = 2.92e-09;
+    const double E_NoC = 2.85e-12;
     //Energy in J spent in tranfering a bit from one port of switch to another
-    const double E_HTree = 5.1e-24;
+    const double E_HTree = 5.1e-15;
     //Energy in J spent in tranfering a bit from one port of switch to another
-    const double E_HTreeRoot = 5.04e-24;
+    const double E_HTreeRoot = 5.04e-15;
     //Energy in J spent in transposing data per bit (average)
-    const double E_Transpose = 1.28e-21;
+    const double E_Transpose = 2.45e-12;
     //Energy in J spent in DRAM controller
     //We don't have a model for this. The area of the chip occupied by
     //the memory controller is about 4%.
@@ -62,15 +62,15 @@ public:
     //add 12% to it .
     const double E_DramCtrl = 0;
 
-    //Static/Leakage energy (per clock)
+    //Static/Leakage energy (per clock) (unit = J)
     const double SE_NoC = 2.21e-10;
     const double SE_HTreeRoot = 1.5e-13;
     const double SE_HTree = 3.64e-14;
-    const double SE_InstrCtrl = 4.69e-14; 
+    const double SE_InstrCtrl = 5.86e-14; 
     const double SE_Transpose = 3.39e-12; 
     const double SE_Popcount = 2.19e-15; 
     const double SE_RF = 2.17e-14; 
-    const double SE_Array = 2.21e-12;  //between rf and noc
+    const double SE_Array = 1.5e-12; //Extrapolated between NoC and RF; The value from OpenRAM was too low.
 
 	double getTiming(Request req);
 	double getDynamicEnergy(Request req);
