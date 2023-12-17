6a0d6db40a81580a3ff46665b92f7b6d8fc12e26
Siyuan Ma <masiyuan0@gmail.com>
2023-12-17 20:49:36 +0800

    adding param_file to  workload func args

--- build/collect_stats.py
+++ build/collect_stats.py
@@ -13,34 +13,101 @@ header = ['name', 'cycle', 'dynamic_energy']
 data = ['', 0, 0]
 
 
-with open('stats.csv', 'w', encoding='UTF8', newline='') as f_stats:
-    writer = csv.writer(f_stats)
+f_stats = open(os.path.join(path,'00_stats.csv'), 'w', encoding='UTF8', newline='')
+writer = csv.writer(f_stats)
+f_breakdown = open(os.path.join(path,'01_breakdown.csv'), 'w', encoding='UTF8', newline='')
+breakdown_writer = csv.writer(f_breakdown)
+# write the header
+writer.writerow(header)
+breakdown_writer.writerow(['Tile','ReqName','Num', 'cycle'])
+f_breakdown_avg = open(os.path.join(path,'02_breakdown_avg.csv'), 'w', encoding='UTF8', newline='')
+breakdown_avg_writer = csv.writer(f_breakdown_avg)
+for filename in sorted(glob.glob(os.path.join(path, '*.log'))):
+    #cycles
+    cycle=0
+    with open(os.path.join(os.getcwd(), filename), 'r') as f:
+        for line in f:
+            if "clocks" in line:
+                words = line.split()
+                cycle = int(words[4])
+                break
+    print(filename+": total cycles: "+str(cycle))
+    #energy
+    energy = 0
+    with open(os.path.join(os.getcwd(), filename+'.energy.csv'), 'r') as f:
+        csv_reader = csv.DictReader(f)
+        for row in csv_reader:
+            #print(f'file {filename}:')
+            energy = row["Total_Dynamic_Energy"]
+            print(f'\t{row["WorkloadName"]} has total dynamic energy = {row["Total_Dynamic_Energy"]}, and total static energy = {row["Total_Static_Energy"]}.')
+    data = [filename, cycle, energy]
+    # write the data
+    writer.writerow(data)
 
-    # write the header
-    writer.writerow(header)
-    for filename in sorted(glob.glob(os.path.join(path, '*.log'))):
-        #cycles
-        cycle=0
-        with open(os.path.join(os.getcwd(), filename), 'r') as f:
-            for line in f:
-                if "clocks" in line:
-                    words = line.split()
-                    cycle = int(words[4])
-                    break
-        print(filename+": total cycles: "+str(cycle))
-        #energy
-        energy = 0
-        with open(os.path.join(os.getcwd(), filename+'.energy.csv'), 'r') as f:
-            csv_reader = csv.DictReader(f)
-            for row in csv_reader:
-                #print(f'file {filename}:')
-                energy = row["Total_Dynamic_Energy"]
-                print(f'\t{row["WorkloadName"]} has total dynamic energy = {row["Total_Dynamic_Energy"]}, and total static energy = {row["Total_Static_Energy"]}.')
-        data = [filename, cycle, energy]
+    #breakdown
+    # header = ['Tile','ReqName', 'cycle']
+    
+
+    tile_count = 0
+    data_list = []
+    idle_cycle = 0
+    with open(os.path.join(os.getcwd(), filename+'.reqs_stats.csv'), 'r') as f:
+        csv_reader = csv.DictReader(f)
+        for row in csv_reader:
+            tile = row["Tile"]
+            reqName = row["ReqName"]
+            if int(row['IDLE'])==1: cycle = int(row["timeTaken"])+int(row['IDLE'])
+            else: cycle = int(row["timeTaken"])-int(row['IDLE'])+1
+            idle_cycle += int(row['IDLE'])-1
+            # print(f'\t{row["WorkloadName"]} has total dynamic energy = {row["Total_Dynamic_Energy"]}, and total static energy = {row["Total_Static_Energy"]}.')
+            present = False
+            match_tile = False
+            for data in data_list:
+                if data[0]==filename and data[1]==tile and data[2]==reqName:
+                    data[3]+=1
+                    data[4]+=int(cycle)
+                    present = True
+                if data[0]==filename and data[1]==tile:
+                    match_tile = True
+            if not present:
+                new_data = [filename, tile, reqName, 1, int(cycle)]
+                data_list.append(new_data)
+            if not match_tile:
+                tile_count+=1
+    print("tile count of "+ filename +" ="+ str(tile_count))
+    
+    for data in data_list:
         # write the data
-        writer.writerow(data)
-        
+        breakdown_writer.writerow(data)
 
+    #average of all tiles involved
+    avg_data_list = []
+    
+    for data in data_list:
+        tile = data[1]
+        reqName = data[2]
+        num = data[3]
+        cycle = data[4]
+        present = False
+        for avg_data in avg_data_list:
+            if avg_data[0]==filename and avg_data[2]==reqName:
+                avg_data[3]+=int(num)
+                avg_data[4]+=int(cycle)
+                present = True
+        if not present:
+            new_data = [filename, "avg of all "+str(tile_count)+" tiles involved", reqName, num, int(cycle)]
+            avg_data_list.append(new_data)
+    
+    for avg_data in avg_data_list:
+        avg_data[3]=round(avg_data[3]/tile_count,2)
+        avg_data[4]=round(avg_data[4]/tile_count,2)
+        # avg_data[3]=round(avg_data[3],2)
+        # avg_data[4]=round(avg_data[4],2)
+        # write the data
+        breakdown_avg_writer.writerow(avg_data)
+    #write average idle cycles
+    breakdown_avg_writer.writerow( [filename,"avg of all "+str(tile_count)+" tiles involved", "IDLE", 1, round(idle_cycle/tile_count,2)])
+    # breakdown_avg_writer.writerow( [filename,"avg of all "+str(tile_count)+" tiles involved", "IDLE", 1, round(idle_cycle,2)])
     
 
 
