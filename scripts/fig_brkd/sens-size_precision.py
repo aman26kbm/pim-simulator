8eb808d1db8acfcfa336e05d3dd0c50d64376eec
kmhatre14 <kmhatre14@gmail.com>
2024-06-16 18:43:32 -0700

    Script to plot Size sensitivity graph

--- scripts/fig_brkd/sens-size_precision.py
+++ scripts/fig_brkd/sens-size_precision.py
@@ -7,8 +7,7 @@ import operator
 import matplotlib.gridspec as gridspec
 import subprocess
 
-raw = pandas.read_csv('scripts/fig_brkd/sensitivity_size_precision.csv')
-# raw = pandas.read_csv('./sensitivity_size_precision.csv')
+raw = pandas.read_csv('./sensitivity_size_precision.csv')
 
 matplotlib.rcParams['lines.linewidth'] = 2.5
 matplotlib.rcParams['text.usetex'] = True
@@ -22,13 +21,13 @@ space = 1
 height = 4
 
 
-fig = plot.figure(figsize=(12,5))
-gs = gridspec.GridSpec(2, 11)
+fig = plot.figure(figsize=(12,8))
+gs = gridspec.GridSpec(10, 3)
 
 axes = []
 
-axes.append(fig.add_subplot(gs[0:1,0:4]))
-axes.append(fig.add_subplot(gs[0:1,6:11]))
+axes.append(fig.add_subplot(gs[0:4,0:2]))
+axes.append(fig.add_subplot(gs[5:11,0:2]))
 
 def plot_bars(ax, data, labels, idx, title, bbox, ylims, color):
     res = []
@@ -50,14 +49,14 @@ def plot_bars(ax, data, labels, idx, title, bbox, ylims, color):
     ax.xaxis.grid(False)
     ax.set_axisbelow(True)
     ax.set_xticks(xs + n // 2)
-    ax.set_xticklabels(['vecadd', 'fir', 'gemv', 'gemm', 'conv2d'], rotation=90, fontsize=12)
+    ax.set_xticklabels(['vecadd', 'fir', 'gemv', 'gemm', 'conv2d'], rotation=90, fontsize=18)
     ax.set_yticks([i for i in np.arange(ylims[0], ylims[1]+0.5,0.5)])
     ax.set_yticklabels(['%.1f' % i for i in np.arange(ylims[0], ylims[1]+0.5,0.5)])
     if idx == 0:
-        ax.legend(handlelength=1, frameon=False, ncol=3, handletextpad=0.5, labelspacing=0.2, loc='upper center', bbox_to_anchor=bbox, fontsize=9)
+        ax.legend(handlelength=1, frameon=False, ncol=3, handletextpad=0.5, labelspacing=0.2, loc='upper center', bbox_to_anchor=bbox, fontsize=15)
     if idx == 1: 
-        ax.legend(handlelength=1, frameon=False, ncol=5, columnspacing=0.5, handletextpad=0.2, labelspacing=0.2, loc='upper center',bbox_to_anchor=bbox, fontsize=9)
-    ax.set_title(title, fontsize=13)
+        ax.legend(handlelength=1, frameon=False, ncol=5, columnspacing=1.5, handletextpad=0.2, labelspacing=0.2, loc='upper center',bbox_to_anchor=bbox, fontsize=15)
+    ax.set_title(title, fontsize=18)
 
     return res
 
@@ -67,7 +66,7 @@ bars = [raw['half'],raw['base'],  raw['double']]
 labels = ['half',
           'main',
           'double']
-gms += plot_bars(axes[0], bars, labels, 0, '(a) Data Size Sens.', (0.5, 1.05), (0, 2.5), ['w', binary(0.45), binary(0.7)])
+gms += plot_bars(axes[0], bars, labels, 0, '(a) Data Size Sens.', (0.5, 1), (0, 2.5), ['w', binary(0.45), binary(0.7)])
 
 
 bars = [raw['int4'],  raw['int5'],  raw['int6'],  raw['int7'],  raw['int8']]
@@ -76,17 +75,17 @@ labels = ['i4',
           'i6',
           'i7',
           'i8']
-gms += plot_bars(axes[1], bars, labels, 1, '(b) Precision Sens.', (0.5, 1.05), (0, 3), ['w', binary(0.45), binary(0.55), binary(0.65), binary(0.75)])
+gms += plot_bars(axes[1], bars, labels, 1, '(b) Precision Sens.', (0.5, 1), (0, 3), ['w', binary(0.45), binary(0.55), binary(0.65), binary(0.75)])
 
 
 
 print(gms)
 
-axes[0].set_ylabel('Relative Exec. Time \n Normalized to Main')
-axes[1].set_ylabel('Relative Exec. Time \n Normalized to i4')
+axes[0].set_ylabel('Relative Exec. Time \n Normalized to Main',fontsize=18)
+axes[1].set_ylabel('Relative Exec. Time \n Normalized to i4',fontsize=18)
 
 
-fig.subplots_adjust(top=0.93, bottom=0.08, left=0.1, right=0.53, wspace=0.02, hspace=1)
+fig.subplots_adjust(top=0.93, bottom=0.10, left=0.1, right=0.53, wspace=0.02, hspace=1)
 plot.show()
 
 fname = 'sensitivity_size_precision'
